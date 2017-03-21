d6f4c6359dc552e1e8d8c5b68bcfa03859be5197
Stacie Traill <trail001@umn.edu>
2017-03-20 20:41:30 -0500

    Minor fixes

--- datasync_unres.py
+++ datasync_unres.py
@@ -1,8 +1,9 @@
 '''
 This script produces an .xlsx file and a .txt file for each of the 5 University of Minnesota
-OCLC symbols. Files are to be used for manual processing of records returned as unresolved by
-OCLC Datasync. Inputs required: OCLC Datasync unresolved cross-ref report, One .csv file per
-symbol from Alma Analytics (e.g. 'OCLC Datasync Unresolved processing MNU').
+OCLC symbols (MNU, MLL, MND, MNX, MCR. Files are to be used for manual processing of records 
+returned as unresolved by OCLC Datasync. Inputs required: OCLC Datasync unresolved 
+cross-ref report, One .csv file persymbol from Alma Analytics (e.g. 'OCLC Datasync 
+Unresolved processing MNU').
 '''
 
 import csv

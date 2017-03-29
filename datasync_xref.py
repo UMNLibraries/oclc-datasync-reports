41f89a793c352c19716138d5bf83ef522517e901
straill <straill@gmail.com>
2017-03-29 17:58:57 -0500

    Add ID list output and error handling

--- datasync_xref.py
+++ datasync_xref.py
@@ -1,6 +1,8 @@
 # coding: utf-8
-'''This script creates brief MARC records from the OCLC Datasync report file
-*xrefrept.txt to be used as input for Alma import profile "Add 035 (OCoLC)* numbers to existing records."'''
+'''This script creates two files from the OCLC Datasync report file *xrefrept.txt:
+1)a .txt. file of Alma MMS IDS formatted for input to Alma's set creation job; 
+2) a file of brief MARC records from the OCLC Datasync report file *xrefrept.txt 
+to be used as input for Alma import profile Add 035 (OCoLC)* numbers to existing records.'''
 
 import csv
 import pymarc
@@ -8,48 +10,53 @@ import os
 from datetime import date
 
 def xref_rpt_parse(xreffile):
-    f = open(xreffile, 'r')
-    file = f.read()
-    lines = file.split('\n')
-
-    xref_parsed = []
-    for line in lines:
-        xref = line.split('\t')
-        xref_parsed.append(xref)
-        
-    with open('upd_xref_out.mrc', 'wb') as processed:
+	today = str(date.today())
+	
+	f = open(xreffile, 'r')
+	file = f.read()
+	lines = file.split('\n')
+	
+	xref_parsed = []
+	for line in lines:
+			xref = line.split('\t')
+			xref_parsed.append(xref)
+		
+	with open('upd_xref_' + today + '.mrc', 'wb') as processed, open('upd_mmsids_' + today + '.txt', 'w') as mmsid_file:
+	
 		rec_count = 0
-    	for row in xref_parsed:
+		mmsid_file.write('MMS ID\n')
 
-            brief_record = pymarc.Record(to_unicode=True, force_utf8=True)
-
-            mmsid = row[0]
-            newocn = row[1]
-
-            field_001 = pymarc.Field(tag='001', data=mmsid)
-            field_035 = pymarc.Field(
-                    tag = '035',
-                    indicators = [' ',' '],
-                    subfields = ['a', '(OCoLC)' + newocn]
-                    )
-            field_245 = pymarc.Field(
-                    tag = '245',
-                    indicators = [' ',' '],
-                    subfields = ['a', 'Title']
-                    )
-            brief_record.add_ordered_field(field_001)
-            brief_record.add_ordered_field(field_035)
-            brief_record.add_ordered_field(field_245)
-
-            processed.write(brief_record.as_marc())
-            rec_count += 1
-
-    return(rec_count)
+		for row in xref_parsed:
+			brief_record = pymarc.Record(to_unicode=True, force_utf8=True)
+			try:
+				mmsid = row[0]
+				newocn = row[1]
+				field_001 = pymarc.Field(tag='001', data=mmsid)
+				field_035 = pymarc.Field(
+					tag = '035',
+					indicators = [' ',' '],
+					subfields = ['a', '(OCoLC)' + newocn]
+					)
+				field_245 = pymarc.Field(
+					tag = '245',
+					indicators = [' ',' '],
+					subfields = ['a', 'Title']
+					)
+				brief_record.add_ordered_field(field_001)
+				brief_record.add_ordered_field(field_035)
+				brief_record.add_ordered_field(field_245)
+			except IndexError:
+				continue
+			processed.write(brief_record.as_marc())
+			mmsid_file.write(str(mmsid) + '\n')
+			rec_count += 1
+		
+	return(rec_count)
 
 def main():
-        fname = input('File to convert: ')
-        count = xref_rept_parsed(fname)
-        print('Done.', count, 'MARC records in file.')
+	fname = input('File to convert: ')
+	count = xref_rpt_parse(fname)
+	print('Done.', count, 'MARC records in file.')
 
 if __name__=='__main__':
-    main()
+	main()
